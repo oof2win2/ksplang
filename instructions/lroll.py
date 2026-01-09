10b9b77faa48ccbaeca743e3419592bc577afaf5
oof2win2 <honza.koco44@gmail.com>
2026-01-09 11:58:15 +0100

    feat: add lroll

---
+++ instructions/lroll.py
@@ -0,0 +1,26 @@
+from .base_instruction import BaseInstruction
+
+
+class LRollInstruction(BaseInstruction):
+    notation = "lroll"
+
+    @staticmethod
+    def execute(stack: list[int]):
+        n = stack.pop()
+        x = stack.pop()
+
+        # remove the last n items from the stack and the restore their order as in the stack
+        nextn = [stack.pop() for _ in range(n)]
+        nextn.reverse()
+
+        if x < 0:
+            # x is negative, so we multiply by -1 for range to work as intended
+            for _ in range(x * -1):
+                nextn.append(nextn.pop(0))
+                print(nextn)
+        else:
+            for _ in range(x):
+                nextn.insert(0, nextn.pop())
+
+        stack.extend(nextn)
+        return
---
+++ instructions/lroll_test.py
@@ -0,0 +1,32 @@
+from ..execute import execute_program
+from .lroll import LRollInstruction
+
+
+def test_lroll_first():
+    instructions = [LRollInstruction.notation]
+    available_instructions = [LRollInstruction]
+    stack = [1, 2, 3, 4, 1, 4]
+    expected_stack = [4, 1, 2, 3]
+    assert (
+        execute_program(instructions, stack, available_instructions) == expected_stack
+    )
+
+
+def test_lroll_second():
+    instructions = [LRollInstruction.notation]
+    available_instructions = [LRollInstruction]
+    stack = [1, 2, 3, 4, -1, 4]
+    expected_stack = [2, 3, 4, 1]
+    assert (
+        execute_program(instructions, stack, available_instructions) == expected_stack
+    )
+
+
+def test_lroll_third():
+    instructions = [LRollInstruction.notation]
+    available_instructions = [LRollInstruction]
+    stack = [0, 1, 2, 3, 4, 2, 4]
+    expected_stack = [0, 3, 4, 1, 2]
+    assert (
+        execute_program(instructions, stack, available_instructions) == expected_stack
+    )
