ca680754fe83062492e75aa497f83b8cc527af18
oof2win2 <honza.koco44@gmail.com>
2026-01-09 12:33:46 +0100

    feat(instruction): add swap instruction

---
+++ instructions/swap.py
@@ -0,0 +1,21 @@
+from .base_instruction import BaseInstruction
+
+
+class SwapInstruction(BaseInstruction):
+    notation = "swap"
+
+    @staticmethod
+    def execute(stack: list[int]):
+        index = stack.pop()
+        toswap = stack.pop()
+
+        if index < 0:
+            raise IndexError("[swap]: Index to swap with cannot be negative")
+        if index > len(stack):
+            raise IndexError("[swap]: Index to swap with out of bounds")
+
+        tmp = stack[index]
+        stack[index] = toswap
+        stack.append(tmp)
+
+        return
