dd8476ef891c28c64c0dc751f24d17c84c9b2119
oof2win2 <honza.koco44@gmail.com>
2026-01-09 12:54:31 +0100

    feat(instruction): add max instruction

---
+++ instructions/max.py
@@ -0,0 +1,12 @@
+from .base_instruction import BaseInstruction
+
+
+class MaxInstruction(BaseInstruction):
+    notation = "max"
+
+    @staticmethod
+    def execute(stack: list[int]):
+        first = stack.pop()
+        second = stack.pop()
+        stack.append(max(first, second))
+        return
