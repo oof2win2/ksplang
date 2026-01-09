c833bf6ade2a08cce441bcb9d727a11fcbf617fc
oof2win2 <honza.koco44@gmail.com>
2026-01-09 13:17:10 +0100

    feat(instruction): add euclidian modulo instruction

---
+++ instructions/euclid_modulo.py
@@ -0,0 +1,13 @@
+from .base_instruction import BaseInstruction
+
+
+class EuclidianModuloInstruction(BaseInstruction):
+    notation = "%"
+
+    @staticmethod
+    def execute(stack: list[int]):
+        first = stack.pop()
+        second = stack.pop()
+        stack.append(first % abs(second))
+
+        return
