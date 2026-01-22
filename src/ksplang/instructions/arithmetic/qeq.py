71955a4a4ae9879a14fc6a263382f047e70f2cf0
oof2win2 <honza.koco44@gmail.com>
2026-01-22 13:30:34 +0100

    fix(qeq): fix quadratic

--- src/ksplang/instructions/arithmetic/qeq.py
+++ src/ksplang/instructions/arithmetic/qeq.py
@@ -14,13 +14,29 @@ class QuadraticSolutionInstruction(BaseInstruction):
         b = executor.stack_pop()
         c = executor.stack_pop()
 
+        if a == 0 and b == 0:
+            # c == 0 equation, infinite solutions
+            if c == 0:
+                raise ValueError("[qeq] The equation is indeterminate.")
+            # for c != 0, there are no solutions
+            return
+
+        if a == 0:
+            # linear equation
+            sol = -c / b
+            if sol.is_integer():
+                executor.stack_push(int(sol))
+            return
+
         sols = [
             (-b + sqrt(b**2 - 4 * a * c)) / (2 * a),
-            (-b + sqrt(b**2 - 4 * a * c)) / (2 * a),
+            (-b - sqrt(b**2 - 4 * a * c)) / (2 * a),
         ]
         sols.sort()
+        # if only one root, remove the duplicate
+        if sols[0] == sols[1]:
+            sols = [sols[0]]
         for sol in sols:
             if sol.is_integer():
                 executor.stack_push(int(sol))
-
         return
--- src/ksplang/instructions/arithmetic/qeq_test.py
+++ src/ksplang/instructions/arithmetic/qeq_test.py
@@ -1,57 +1,34 @@
+import pytest
+
 from .qeq import QuadraticSolutionInstruction
 
 
-def test_qeq_two_integer_solutions(execute_single_instruction):
-    assert execute_single_instruction(QuadraticSolutionInstruction, [6, -5, 1]) == [
-        3,
-        3,
-    ]
+@pytest.mark.parametrize(
+    "a,b,c,sols",
+    [
+        # (x + 5)(x - 8)
+        # x^2 - 3x - 40 = 0
+        (-40, -3, 1, [-5, 8]),
+        # (x - 3)(x - 3) -> only one root, 3
+        (9, -6, 1, [3]),
+        # no real solutions
+        (-7, 3, 1, []),
+    ],
+)
+def test_qeq_quadratic(a, b, c, sols, execute_single_instruction):
+    assert execute_single_instruction(QuadraticSolutionInstruction, [a, b, c]) == sols
 
 
-def test_qeq_one_integer_solution(execute_single_instruction):
-    assert execute_single_instruction(QuadraticSolutionInstruction, [1, -2, 1]) == [
-        1,
-        1,
-    ]
-
-
-def test_qeq_negative_coefficients(execute_single_instruction):
-    assert execute_single_instruction(QuadraticSolutionInstruction, [9, 0, -1]) == [
-        -3,
-        -3,
-    ]
-
-
-def test_qeq_large_coefficients(execute_single_instruction):
-    assert execute_single_instruction(QuadraticSolutionInstruction, [16, -10, 1]) == [
-        8,
-        8,
-    ]
-
-
-def test_qeq_no_real_solutions(execute_single_instruction):
-    assert execute_single_instruction(QuadraticSolutionInstruction, [-1, 0, 1]) == [
-        1,
-        1,
-    ]
-
-
-def test_qeq_duplicate_solutions(execute_single_instruction):
-    assert execute_single_instruction(QuadraticSolutionInstruction, [9, -6, 1]) == [
-        3,
-        3,
-    ]
-
-
-def test_qeq_negative_solution(execute_single_instruction):
-    assert execute_single_instruction(QuadraticSolutionInstruction, [12, 13, 1]) == [
-        -1,
-        -1,
-    ]
-
-
-def test_qeq_simple(execute_single_instruction):
-    assert execute_single_instruction(QuadraticSolutionInstruction, [-4, 0, 1]) == [
-        2,
-        2,
-    ]
+@pytest.mark.parametrize(
+    "a,b,c,sols",
+    [
+        # 2x+4 = 0 -> x = -2
+        (4, 2, 0, [-2]),
+        # 2x + 3 = 0 -> x not an int
+        (3, 2, 0, []),
+        # 4x = 0 -> no sols
+        (4, 0, 0, []),
+    ],
+)
+def test_qeq_linear(a, b, c, sols, execute_single_instruction):
+    assert execute_single_instruction(QuadraticSolutionInstruction, [a, b, c]) == sols
