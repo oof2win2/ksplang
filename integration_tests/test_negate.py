5b4fbb951374490700340c1039fdd37539a8ea60
oof2win2 <honza.koco44@gmail.com>
2026-01-22 13:30:55 +0100

    test(integration): negate a number

---
+++ integration_tests/test_negate.py
@@ -0,0 +1,21 @@
+from ksplang.execute import discover_instructions
+from ksplang.executor import Executor
+
+available_instructions = discover_instructions()
+
+# the stack is not empty, there's always at least one number on it
+# the stack should
+
+
+def test_negate_one():
+    startstack = [5]
+    endstack = [-5]
+
+    executor = Executor(
+        "CS CS lensum CS funkcia ++ CS CS % qeq".split(" "),
+        startstack,
+        available_instructions,
+    )
+    executor.execute_program()
+    # there should be a zero at the end
+    assert endstack == executor.get_stack()
